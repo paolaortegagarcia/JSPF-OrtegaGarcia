b5f61af759384025d70c0ccc9dc058a6b26c35d0
Paola Ortega <pao.ortega.95@gmail.com>
2023-06-25 16:41:42 -0300

    agregado de fetch y JSON para proyecto final

--- js/script.js
+++ js/script.js
@@ -12,18 +12,6 @@ class Juegos {
     constructor(nombreJuego, nombreJugador) {
         this.nombreJuego = nombreJuego;
         this.nombreJugador = nombreJugador;
-        this.instruccionesJuego = {
-            ppt: `
-                <p>Debes elegir entre piedra, papel o tijera.</p><p>Cuando alguno de los dos jugadores llegue a 3 partidas ganadas, el juego terminará.</p>
-                            <p>- La piedra aplasta las tijeras.</p> 
-                            <p>- El papel envuelve la piedra.</p> 
-                            <p>- las tijeras cortan el papel.</p>
-            `,
-            a: `
-                <p>Debes adivinar la palabra antes de que se completen 10 rondas.
-                            <p>Adivina las letras una por una, si está presente en la palabra, aparecerá en su posicion correspondiente
-            `,
-        };
     }
 
     mostrarNombre() {
@@ -51,29 +39,41 @@ class Juegos {
     }
 
     mostrarInstrucciones() {
+        
         const divJuego = document.getElementById("juegoElegido");
 
-        divJuego.innerHTML = `
-            <h2 class="tituloJuego">${nombresJuegos[this.nombreJuego]}</h2>
-            <div class="inst">
-                <p class="instruccionesJuego">${this.nombreJugador}, ${this.instruccionesJuego[this.nombreJuego]}</p>
-                <button id="jugarbtn">Jugar</button>
-            </div>
-        `;
-        let jugarbtn = document.getElementById("jugarbtn");
+        fetch("./js/instrucciones.json")
+        .then(response => response.json())
+        .then(data => {
+            const instrucciones = data[this.nombreJuego];
 
-        jugarbtn.addEventListener("click", () => {
-            jugarbtn.disabled = false;
-            this.iniciarJuego();
-            jugarbtn.disabled = true;
+            divJuego.innerHTML = `
+                <h2 class="tituloJuego">${nombresJuegos[this.nombreJuego]}</h2>
+                <div class="inst">
+                    <p class="instruccionesJuego">${this.nombreJugador}, ${instrucciones.texto}</p>
+                    ${instrucciones.reglas ? `<ul>${instrucciones.reglas.map(regla => `<li>${regla}</li>`).join("")}</ul>` : ""}
+                    <button id="jugarbtn">Jugar</button>
+                </div>
+            `;
+            
+            let jugarbtn = document.getElementById("jugarbtn");
+
+            jugarbtn.addEventListener("click", () => {
+                jugarbtn.disabled = false;
+                this.iniciarJuego();
+                jugarbtn.disabled = true;
+            });
+
+            let tituloJuego = document.querySelector("#juegoElegido h2");
+            let instruccionesJuego = document.querySelector("#juegoElegido p");
+            tituloJuego.classList.add(this.nombreJuego);
+            instruccionesJuego.classList.add("instruccionesJuego");
+        })
+        .catch(error => {
+            console.error("Error al cargar las instrucciones:", error);
         });
-
-        let tituloJuego = document.querySelector("#juegoElegido h2");
-        let instruccionesJuego = document.querySelector("#juegoElegido p");
-        tituloJuego.classList.add(this.nombreJuego);
-        instruccionesJuego.classList.add("instruccionesJuego");
-
     }
+
 
     iniciarJuego() {
         if (this.nombreJuego === "ppt") {
@@ -93,7 +93,7 @@ class Juegos {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    let nombreJugador = localStorage.getItem("nombreJugador");
+    
     const juegosDisponibles = document.querySelector(".juegosDisponibles");
     const juego = document.querySelectorAll(".juego");
 
@@ -102,6 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
     juego.forEach((link) => {
         link.addEventListener("click", () => {
             let nombreJuego = link.classList[1];
+            let nombreJugador = localStorage.getItem("nombreJugador");
             const juegos = new Juegos(nombreJuego, nombreJugador);
             
             if (nombreJuego === "ttt") {
@@ -117,7 +118,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    const juegos = new Juegos(nombreJugador);
+    let nombreJugador = localStorage.getItem("nombreJugador");
+    const juegos = new Juegos("",nombreJugador);
     juegos.mostrarNombre();
 });
 
